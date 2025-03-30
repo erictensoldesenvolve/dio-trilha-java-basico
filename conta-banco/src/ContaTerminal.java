c925a1861f81394867a124d16668275dfec814d2
Eric Tensol Vieira <62815764+erictensoldesenvolve@users.noreply.github.com>
2025-03-30 17:08:55 -0300

    criei os arquivos

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,28 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+       
+       
+        System.out.println("Olá, Digite seu nome.");
+        String nome = scanner.next();
+
+        
+        System.out.println("Digite o número da conta.");
+        int numero = scanner.nextInt();
+
+        System.out.println("Digite a Agência.");
+        String agencia = scanner.next();
+
+        System.out.println("Digite seu saldo.");
+        double saldo = scanner.nextDouble();
+
+
+
+        System.out.println("Olá "+ nome + ", obrigado por criar uma conta em nosso banco," +
+        " sua agência é" + agencia + ", conta " + numero + "e seu saldo " + saldo +  
+        "já está disponível para saque");
     }
 }
